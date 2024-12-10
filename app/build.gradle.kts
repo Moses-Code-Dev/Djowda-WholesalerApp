a86ff1e1dc8303a72b9c308226f46265c36173e3
Andoid <mosescodedev@gmail.com>
2024-12-10 11:34:56 +0100

    initial setup.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,22 @@ plugins {
 
 android {
     namespace = "com.djowda.djowdaWholeSeller"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.djowda.djowdaWholeSeller"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments["room.schemaLocation"] = "$projectDir/schemas"
+            }
+        }
     }
 
     buildTypes {
@@ -26,8 +32,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
@@ -37,6 +43,20 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
+    implementation(libs.glide)
+
+    annotationProcessor(libs.compiler)
+
+    implementation(libs.core.splashscreen)
+    implementation(libs.room.runtime)
+
+    annotationProcessor(libs.room.compiler)
+
+    implementation(libs.play.services.location)
+    implementation(libs.gson)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
